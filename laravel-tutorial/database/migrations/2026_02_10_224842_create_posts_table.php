6441092086722c3fe9140dc93b07d7d5f5e86605
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-02-11 16:43:35 +0200

    validate our request and move it to seperate file & making migration and save info into database by doing crud

---
+++ laravel-tutorial/database/migrations/2026_02_10_224842_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;  
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            // this used for relation between user table that laravel provide and any other table
+            $table->foreignIdFor(User::class, 'author_id')->constrained();
+            // this is used when other table relations
+            // $table->foreignId('author_id')
+            // ->references('users')
+            // ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
